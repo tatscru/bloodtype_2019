51c3b5098f457bda7abd8e321080103104a07c48
tatiana.mojica <tatiana.mojica@bustle.com>
2019-01-02 21:50:33 +0000

    first commit

---
+++ lib/bloodtype_2019.rb
@@ -0,0 +1,6 @@
+require "bloodtype_2019/version"
+
+module Bloodtype2019
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bloodtype_2019/version.rb
@@ -0,0 +1,3 @@
+module Bloodtype2019
+  VERSION = "0.1.0"
+end
