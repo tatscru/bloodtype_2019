0978bc52b8f35c6c6ea80961a308120f510822e4
tatiana.mojica <tatiana.mojica@bustle.com>
2019-01-03 05:33:33 +0000

    code that works

--- lib/bloodtype_2019/cli.rb
+++ lib/bloodtype_2019/cli.rb
@@ -3,48 +3,46 @@
 class Bloodtypes::CLI
 
   def call 
-    puts "The 4 Different Blood Types"
-    # lists_blood_types
-
+    lists_blood_types
+    foods 
+    goodbye
   end 
 
-
-#   def lists_blood_types
-
-#     puts "The 4 different blood types"
-
-#     1. Type O
-#     2. Type A
-#     3. Type B
-#     4. Type AB
-
-#     foods
-#   end 
+  def lists_blood_types
+    puts "The 4 Different Blood Types:"
+    puts <<DOC.gsub /^\s*/,"" 
+    1. Type O
+    2. Type A
+    3. Type B
+    4. Type AB
+DOC
+  end 
   
-#   def foods 
-#     puts "To learn more, enter the number associated with your blood type, enter list to see the list of bloodtypes again, or type exit" 
-  
-#     input = gets.strip 
-#     case = input 
-#     when "1"
-#       puts "More info on Type O:"
-#     when "2"
-#       puts "More info on Type A"
-#     when "3" 
-#       puts "More info on Type B"
-#     when "4" 
-#       puts "More info on Type AB"
-#     else 
-#       puts "Sorry, I did not understand your input"
-#   end 
-# end 
-  
-  
+  def foods 
+    puts "To learn more, enter the number associated with your blood type or type exit" 
+    input = nil 
+    while input != "exit"
+      input = gets.strip 
+      case input 
+        when "1"
+         puts "More info on Type O..."
+        when "2"
+          puts "More info on Type A..."
+        when "3" 
+          puts "More info on Type B..."
+        when "4" 
+         puts "More info on Type AB..."
+        else 
+          puts "Sorry, I did not understand your input. Enter a number 1-4 or type exit"
+      end 
+    end 
+  end 
+
 #   def sample_menu
 #   end 
 
-#   def goodbye
-#     puts "Have a wonderful day!"
-#   end 
+  def goodbye
+    puts "Have a wonderful day!"
+  end 
 
 end 
