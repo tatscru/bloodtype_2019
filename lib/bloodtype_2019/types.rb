22dd408dfdc6ba8a87ab37e1fd060d84e2f5914e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-01-03 05:49:40 +0000

    types file

--- lib/bloodtype_2019/types.rb
+++ lib/bloodtype_2019/types.rb
@@ -1,31 +1,31 @@
-# class Bloodtype::TYPES
+class Bloodtypes::TYPES
 
-#   attr_accessor :name, :foods, :menu 
+  attr_accessor :name, :foods, :menu 
 
-#   def self.types 
+  def self.types 
   
-#     type_o = self.new 
-#     type_o.name = Blood Type O 
-#     type_o.foods = 
-#     type_o.menu = 
+    type_o = self.new 
+    type_o.name = "Blood Type O" 
+    type_o.foods = 
+    type_o.menu = 
 
-#     type_a = self.new 
-#     type_a.name = Blood Type O 
-#     type_a.foods = 
-#     type_a.menu = 
+    type_a = self.new 
+    type_a.name = "Blood Type A" 
+    type_a.foods = 
+    type_a.menu = 
 
-#     type_b = self.new 
-#     type_b.name = Blood Type O 
-#     type_b.foods = 
-#     type_b.menu = 
+    type_b = self.new 
+    type_b.name = "Blood Type B" 
+    type_b.foods = 
+    type_b.menu = 
 
-#     type_ab = self.new 
-#     type_ab.name = Blood Type O 
-#     type_ab.foods = 
-#     type_ab.menu = 
+    type_ab = self.new 
+    type_ab.name = "Blood Type AB" 
+    type_ab.foods = 
+    type_ab.menu = 
   
   
-#   [type_o, type_a, type_b, type_ab]
-#   end 
+  [type_o, type_a, type_b, type_ab]
+  end 
   
-# end 
+end 
