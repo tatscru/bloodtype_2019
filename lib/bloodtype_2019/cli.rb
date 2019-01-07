e2c4ebaee0558a5f25e08e3eb0502402929dce56
tatiana.mojica <tatiana.mojica@bustle.com>
2019-01-07 19:12:37 +0000

    types and changes

--- lib/bloodtype_2019/cli.rb
+++ lib/bloodtype_2019/cli.rb
@@ -5,8 +5,8 @@ class CLI
   def call 
     lists_blood_types
     prompt
-    sample_menu
-    goodbye
+    # sample_menu
+    # goodbye
    
   end 
 
@@ -21,36 +21,36 @@ class CLI
     # binding.pry 
   end 
   
-  def prompt
+   def prompt
     puts "To learn more, type the name of your blood type or type list to output the bloodtype names again" 
-    # sample_menu()
     
-    input = nil 
-    while input != "exit"
-      input = gets.strip.upcase 
-      case input 
-        when "TYPE O"
-         puts Types.types_array[1]
-        # sample_menu()
-        when "TYPE A"
-          puts Types.types_array[3]
-        when "TYPE B" 
-          puts Types.types_array[5]
-        when "TYPE AB" 
-          puts Types.types_array[7]
-        when "LIST"
-          puts lists_blood_types
-        when "EXIT"
-          puts goodbye 
-        else 
-          puts "Sorry, I did not understand your input. Reenter your bloodtype, type list to see the different bloodtypes again, or type exit"
-      end 
-    end 
+    case @type = gets.strip.downcase
+      when "type o"
+        puts Types.types_array[1]
+      when "type a"
+        puts Types.types_array[3]
+      when "type b"
+        puts Types.types_array[5]
+      when "type ab" 
+        puts Types.types_array[7]
+      when "list"
+        puts lists_blood_types
+      when "exit"
+        puts goodbye 
+      else 
+        puts "Sorry, I did not understand your input. Reenter your bloodtype, type list to see the different bloodtypes again, or type exit"
+    end
   end 
     
-  def sample_menu(type)
-    Y/N
-  end 
+  def sample_menu
+    puts "Would you like to see a sample menu for #{@type}?"
+    response = gets.downcase.strip
+    if response.eql?('yes')
+      Types.get_sample_menus.each do |meal, menu|
+        puts meal
+        puts menu
+      end
+    end
 
   def goodbye
     puts "Have a wonderful day!"
